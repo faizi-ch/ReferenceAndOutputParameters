56c92daed18731a6fdd46a044e12be9618bdec0e
faizi-ch <faizi.ch@live.com>
2018-08-03 21:07:34 +0500

    Add project files.

---
+++ ReferenceAndOutputParameters/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Xml;
+
+
+namespace ReferenceAndOutputParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b;
+
+            Console.WriteLine("square ref: {0}",FindSquareRef(ref a));
+            Console.WriteLine("find square out: {0}", FindSquareOut(out b));
+            Console.WriteLine("{0}\n{1}", FindSquare(a), FindSquare(b));
+            Console.ReadKey(true);
+
+        }
+
+        static int FindSquareRef(ref int n)
+        {
+            return n*n;
+        }
+
+        static int FindSquareOut(out int n)
+        {
+            n = 6;
+            return n*n;
+        }
+
+        static int FindSquare(int n)
+        {
+            return n*n;
+        }
+    }
+}
